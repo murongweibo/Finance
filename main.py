78e185dd18e2015dbf2bb8aeadb15b937584f4e7
murongweibo <15814810069@139.com>
2024-10-11 17:33:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -14,12 +14,13 @@ def get_data():
     if api_key == 'murongweibo':
         today = str(pd.Timestamp.now()).split()[0]
         data_list = []
-        for symble, symble_cn in [("^NDX", "Nasdaq100"),
-                                   ("^GSPC", "S&P500"),
-                                   ("000300.SS", "CSI300"),
+        for symble, symble_cn in [("^NDX", "纳斯达克100"),
+                                   ("^GSPC", "标准普尔500"),
+                                   ("000300.SS", "沪深300"),
                                    ]:
             symbol = yf.Ticker(symble)
             hist = symbol.history(start="2020-01-01", end=today)
+            hist["time"] = hist.index.map(lambda x: str(x).split()[0])
             hist["yesterday_close"] = hist.Close.shift(1)
             hist["today_close"] = hist.Close
             hist["rolling_mean"] = hist.Close.rolling(240).mean()
@@ -27,6 +28,7 @@ def get_data():
             hist = hist.fillna(0)[-1:]
             data_dict = {
                 "symbol": symble_cn,
+                "time" : hist["time"].iloc[0],
                 "yesterday_close": round(hist["yesterday_close"].iloc[0], 2),
                 "today_close": round(hist["today_close"].iloc[0], 2),
                 "growth_factor": round(hist["growth_factor"].iloc[0], 2)
